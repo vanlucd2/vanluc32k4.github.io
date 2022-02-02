c469c796dfb78f6cf54640fc50d9d397b6f4dcaf
vanlucd2 <98869129+vanlucd2@users.noreply.github.com>
2022-02-02 18:59:32 +0700

    123

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 hí Huyền=)) ! 🖐🖐🖐',
+    introDesc: `Huyền ơi tao có điều muốn nói, mày nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK con đĩ lực=))',
+    title: 'mày là con đĩ của tao phải không=)) ? 😙',
+    desc: 'Mày không trả lời hoặc thoát ra, thì mày là con đĩ của tao rồi nha :> ',
+    btnYes: 'Tao là con đĩ của mày=))',
+    btnNo: '💔 mày mơ à=)) ! 💔 ',
+    question:'Nói cho tao biết đi vì mày là con đĩ của tao đi=)) 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì tao yêu mày đó Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    
+}
